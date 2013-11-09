c627818230336b9f7e47648ea29624e123c24d0f
Timothee <t1m95.tb@gmail.com>
2013-11-09 15:11:25 +0100

    Signed-off-by: Timothee <t1m95.tb@gmail.com>

--- src/js/case.js
+++ src/js/case.js
@@ -3,8 +3,8 @@ var Case = function(x,y,color) { //couleur de la forme #ffffff
 	this.x     = x; //coordonné abscisse
 	this.y     = y; //coordonné ordonnné (x,y)
 	this.color = color;
-	this._init.apply(this,arguments);
 	this.state = { element: "empty", side: "none" }; //détermine si un pion dessus et quel coté il est
+	this._init.apply(this,arguments);
 }
 
 Case.prototype = {
@@ -13,7 +13,7 @@ Case.prototype = {
 		if(arguments.lenght! = 1 && isNaN(argmuments[0]) throw new Error('Bad arguments, expected 1, had '+arguments.lenght);
 		if(color) this.color = '#C1B4D6';
 		var case             = '<div class="case" id="x'+x+'y'+y+'" style="color:'+this.color+';heigh:50px;width:50px"></div>';
-		return $(".damier").appendChild(case);
+		return $(".damier").append(case);
 	},
 	hasElement: function() { //pour déterminer si un élément est positionné sur la case
 		var coord = 'x'+this.x+'y'+this.y;
@@ -21,9 +21,14 @@ Case.prototype = {
 	}
 	updateState: function(element,side) {
 		if(argmuments.length != 2) throw new Error('Bad number of arguments, expected 2, had '+arguments.lenght);
+		if(element != ('empty' || 'pion' || 'dame') ) throw new Error('Expected empty or pion or dame, had '+element);
+		if(side != ('blanc' || 'noir' || 'none' ) ) throw new Error('Expected blanc or noir or none, had '+side);
 		return {
-			this.state.element = element;
-			this.state.side = side;
+			this.state.element = element; //pion, dame ou empty
+			this.state.side = side; //blanc ou noir ou none
 		}
+	}
+	getState : function () { //attention : il faudra surement faire un apply()
+		return this.state.element+':'+this.state.side;
 	}
 }
