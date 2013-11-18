cf3d1e7702a20874e05b9a5cb00cb4e5f9d022a3
Timothee <t1m95.tb@gmail.com>
2013-11-18 12:00:57 +0100

    Signed-off-by: Timothee <t1m95.tb@gmail.com>

--- js/mainDamier.js
+++
@@ -1,8 +0,0 @@
-/* MAIN */
-
-$(document).ready(function() {
-	var player = new Player();
-	//console.log(player.side);
-	var damier = new Damier(player.side);
-	damier.updatePossibleMooves.apply(damier);
-});
